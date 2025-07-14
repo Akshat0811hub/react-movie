086c703bd4a2e6b67543e643fc0a91aaddf3d958
Akshat0811 <singhakshat875@gmail.com>
2025-07-14 23:54:34 +0530

    done

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "086cfe05dd16828e37291d2f37293a38",
+  apiKey: process.env.REACT_APP_TMDB_API_KEY,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
